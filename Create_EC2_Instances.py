a56c237bb6e1241ca6bb06dc0137c3c10277dc7e
Anburaj2202 <36660890+Anburaj2202@users.noreply.github.com>
2022-12-22 14:50:58 +0530

    Update Create_EC2_Instances.py

--- Create_EC2_Instances.py
+++ Create_EC2_Instances.py
@@ -1,15 +1,16 @@
 import boto3
+import os
 
 # AMI_ID = (str(input("Enter the Instance ID: ")))
 # Instance_Numbers = (int(input("Enter the Number of the Instnces: ")))
-# AMI_ID = str()
-# Instance_Numbers = int()
+AMI_ID = str(os.getenv("AMI_ID"))
+Instance_Numbers = int(os.getenv("Quantity"))
 # print(AMI_ID,Instance_Numbers)
 # ami-0a261c0e5f51090b1
 # Access_Key = (str(input("Enter the Access Key: ")))
 # Secret_Key = (str(input("Enter the Secret Key: ")))
-# Access_Key = str()
-# Secret_Key = str()
+Access_Key = str(os.getenv("Access_Key"))
+Secret_Key = str(os.getenv("Secret_Key"))
 ec2 = boto3.resource('ec2',region_name = "eu-central-1",aws_access_key_id = Access_Key, aws_secret_access_key = Secret_Key)
 instances = ec2.create_instances(
         ImageId= AMI_ID,
